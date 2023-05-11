450e10a5cff1fbda9580edb9fee89ea72096acad
kuangbo <894343254@qq.com>
2023-05-11 14:44:47 +0800

    npm发包调整、引入commander制成工具、bug fix

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
+#! node
+
 const path = require("path");
+const { program } = require("commander");
+program
+  .name("create-app")
+  .usage("[options] projectName")
+  .description(
+    "创建webpack、javascript/vue/react、less/scss、typescript、babel、eslint相关项目基础，\n省去了项目搭建的一些重复安装步骤、配置文件等"
+  )
+  .version(require("./package.json").version)
+  .option(
+    "-r, --registry <url>",
+    "设置npm下载镜像源,默认：https://registry.npm.taobao.org",
+    "https://registry.npm.taobao.org"
+  )
+  .argument("<appName>", "待创建项目的名称")
+  .action(async (appName, options) => {
+    const { default: chalk } = await import("chalk");
 
-(async function () {
-  const { default: chalk } = await import("chalk");
+    const cwd = path.resolve(__dirname, appName);
 
-  const cwd = path.resolve(__dirname, "test");
+    const log = console.log.bind(console);
+    const info = (...args) => {
+      console.log(chalk.green(...args));
+    };
+    const error = (...args) => {
+      console.error(chalk.red(...args));
+    };
 
-  const log = console.log.bind(console);
-  const info = (...args) => {
-    console.log(chalk.green(...args));
-  };
-  const error = (...args) => {
-    console.error(chalk.red(...args));
-  };
+    const logUtils = {
+      log,
+      info,
+      error,
+    };
 
-  const logUtils = {
-    log,
-    info,
-    error,
-  };
+    async function run() {
+      //根据用户查询下载npm dev包
+      const userOptions = await require("./lib/devInstall")({
+        cwd,
+        logUtils,
+        launch_options: options,
+      });
+      //添加各种配置文件
+      await require("./lib/config")({
+        cwd,
+        logUtils,
+        userOptions,
+      });
+      await require("./lib/addScripts")({ cwd, logUtils });
+    }
 
-  async function run() {
-    //根据用户查询下载npm dev包
-    const userOptions = await require("./lib/devInstall")({ cwd, logUtils });
-
-    //添加各种配置文件
-    await require("./lib/config")({
-      cwd,
-      logUtils,
-      userOptions,
-    });
-
-    await require("./lib/addScripts");
-  }
-
-  try {
-    await run();
-  } catch (e) {
-    error(e);
-  }
-})();
+    try {
+      await run();
+    } catch (e) {
+      error(e);
+    }
+  });
+program.parse();
